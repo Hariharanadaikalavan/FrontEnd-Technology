726f4f21c9394822d8e65d461ef0aec0f9db3597
hariharanadaikalavan <hari.ramu16@gmail.com>
2023-10-16 22:38:55 +0530

    Oparators

---
+++ JavaScript/OperatorsStatement/app.js
@@ -0,0 +1,14 @@
+var anchoviesPizzaScore = 0;
+var  pepperoniPizzaScore = 4;
+var  hawaiianPizzaScore = 4;
+var  chickenBaconRanchPizzaScore = 6;
+
+console.log("anchoviesPizzaScore < pepperoniPizzaScore (0<4)-->",(anchoviesPizzaScore < pepperoniPizzaScore));
+
+console.log("chickenBaconRanchPizzaScore > hawaiianPizzaScore (6>4)-->",(chickenBaconRanchPizzaScore > hawaiianPizzaScore));
+
+console.log("hawaiianPizzaScore > pepperoniPizzaScore(4>4)",(hawaiianPizzaScore > pepperoniPizzaScore));
+
+console.log("pepperoniPizzaScore <= chickenBaconRanchPizzaScore(4<=6)-->",(pepperoniPizzaScore <= chickenBaconRanchPizzaScore));
+
+console.log("hawaiianPizzaScore >= pepperoniPizzaScore(4>=4)-->",(hawaiianPizzaScore >= pepperoniPizzaScore));
