c585851fab01f2976b176b59fd510f7c39062196
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2023-10-23 23:46:30 +0530

    Object

---
+++ JavaScript/bahubali.js
@@ -0,0 +1,13 @@
+var movie={
+    title:"Baahubali",
+    director:"Rajamouli",
+    producers:["Sobhu","Raghavendra","Prasad"],
+ Lead_actors_with_their_roles:
+{
+    prabhas:"baahubali",
+    anushka:'devasena',
+    rana:'bhallaladeva',
+    tamannah:'Avanthika',
+}
+}
+console.log(movie);
