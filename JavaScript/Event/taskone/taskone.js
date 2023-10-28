ff213b14f5eb2fe3a9fcc03f51ee9fb07a38e210
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2023-10-28 22:50:44 +0530

    oneclick onkeyup event

---
+++ JavaScript/Event/taskone/taskone.js
@@ -0,0 +1,9 @@
+let label1=document.createElement('label');
+let txt=document.createTextNode("userName");
+label1.appendChild(label1);
+document.body.appendChild(label1);
+let uname=document.createElement('input');
+uname.setAttribute('type','text');
+uname.setAttribute('id','uname');
+label1.appendChild('uname');
+document.body.appendChild(label1);
