e90330a78eab462ae45eed7087c275e0af4f3890
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2023-11-11 22:08:54 +0530

    Objects

---
+++ JavaScript/vehicledata.js
@@ -0,0 +1,9 @@
+var vehicle={
+    name:"Mercedes Benz",
+    model:"C-Class",
+    manufacture:"Daimler AG Company",
+    length:"4686mm",
+    cargo_capacity:"12.6 Cubic Feet",
+}
+
+console.log(vehicle);
