4d2130694607e01e1efcd23ffba26074ce54acbb
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2023-11-11 22:11:46 +0530

    Math_Exercises

---
+++ JavaScript/randamgenrator.js
@@ -0,0 +1,4 @@
+console.log(Math.random(20,1));
+console.log(Math.random(1,10));
+console.log(Math.random(6));
+console.log(Math.random());
---
+++ JavaScript/tasktwo.js
@@ -0,0 +1,22 @@
+console.log(Math.min(-12,34,56,1));
+console.log(Math.min(34,12,56,1));
+
+// let a=[55,6,7,8,10]
+// console.log(Math.min(...a));
+
+// function panlindrome(str)
+// {
+//     var rev="";
+//     for(var i=str.length-1;i>=0;i--)
+//     {
+//         rev+=str[i];
+//     }
+//   if(str==rev)
+//   {
+//     console.log("panlidrome");
+//   }
+//   else{
+//     console.log("Not a panlidrome")
+//   }
+// }
+// panlindrome("hari")
