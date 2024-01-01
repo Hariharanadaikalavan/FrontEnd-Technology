efbbccb66915171b0d6190e00e158620df5d2488
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2024-01-01 23:43:07 +0530

    Update randamgenrator.js

--- JavaScript/Math_Exercise/randamgenrator.js
+++ JavaScript/Math_Exercise/randamgenrator.js
@@ -1,4 +1,5 @@
-console.log(Math.random(20,1));
+console.log(Math.random(20,1)); //limited values accept 
+
 console.log(Math.random(1,10));
 console.log(Math.random(6));
 console.log(Math.random());
